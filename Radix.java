172b547f6084d40d367c8b80d84aa9cbd1f2cf09
Zhao Yu Lin <>
2021-01-12 12:12:38 -0500

    added nth(), tested it, seems to work

---
+++ Radix.java
@@ -0,0 +1,23 @@
+public class Radix{
+  public static void main(String[] args){
+    System.out.println(nth(123,1));
+    System.out.println(nth(-123,1));
+    System.out.println(nth(123,2));
+    System.out.println(nth(-123,2));
+    System.out.println();
+    System.out.println(length(0));
+    System.out.println(length(15));
+    System.out.println(length(-10));
+    System.out.println(length(11111111));
+
+  }
+  public static int nth(int n, int col){
+    return (int) (Math.abs(n) / Math.pow(10, col)) % 10;
+  }
+
+  
+
+  public static void merge(MyLinkedList original,MyLinkedList[] buckets){
+
+  }
+}
