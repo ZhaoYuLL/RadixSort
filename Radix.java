b1766b18812a6f35492155aa5aaf9522d530fe11
Zhao Yu Lin <>
2021-01-12 19:03:19 -0500

    added loops for how many passess required, trying to figure out how to sort

--- Radix.java
+++ Radix.java
@@ -1,5 +1,5 @@
 public class Radix{
-  
+
   public static int nth(int n, int col){
     return (int) (Math.abs(n) / Math.pow(10, col)) % 10;
   }
@@ -11,7 +11,49 @@ public class Radix{
     return (int) (Math.log10(Math.abs(n)) + 1);
   }
 
-  public static void merge(MyLinkedist original,MyLinkedList[] buckets){
-
+  public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
+    for(int i = 0; i < buckets.length; i ++){
+      original.extend(buckets[i]);
+    }
   }
+  public static void insertionSort(int[] data){
+    for(int i = 1; i < data.length; i ++){
+      int j = i;
+      while(j > 0 && data[j-1]>data[j]){
+        int temp = data[j];
+        data[j]=data[j-1];
+        data[j-1] = temp;
+        j -=1;
+      }
+    }
+  }
+  public static void radixSortSimple(SortableLinkedList data){
+
+    int maxValue = 0;
+    for(int i = 0; i < data.size(); i ++){ //loop through data to get maxValue
+        maxValue = Math.max(data.get(i),maxValue);
+    }
+    int mostDigits = length(maxValue); //e.g 432, mostdigits would be 3
+    for(int i = 0; i < mostDigits; i ++){
+      SortableLinkedList zero = new SortableLinkedList();
+      SortableLinkedList one = new SortableLinkedList();
+      SortableLinkedList two = new SortableLinkedList();
+      SortableLinkedList three = new SortableLinkedList();
+      SortableLinkedList four = new SortableLinkedList();
+      SortableLinkedList five = new SortableLinkedList();
+      SortableLinkedList six = new SortableLinkedList();
+      SortableLinkedList seven = new SortableLinkedList();
+      SortableLinkedList eight = new SortableLinkedList();
+      SortableLinkedList nine = new SortableLinkedList();    //create buckets everytime, first pass for one's digit, 2nd for 2nd, third for 3rd
+    }
+    for(int j = 0; j < data.size(); j ++){ //loop through data to sort
+      int digit = nth(data.get(j),j);  //gets ones place, added according to where. This is a stable sort too
+      for(int i = 0; i <= 9; i++){
+        if(digit == i){
+
+        }
+      }
+    }
+  }
+
 }
