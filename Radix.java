8d6121ffcfbfaf78259b5701f4d879828c31d4cb
Zhao Yu Lin <>
2021-01-13 17:54:09 -0500

    optimized radixSortSimple by using loops insteaed.

--- Radix.java
+++ Radix.java
@@ -1,74 +1,74 @@
-public class Radix{
+  public class Radix{
 
-  public static int nth(int n, int col){
-    return (int) (Math.abs(n) / Math.pow(10, col)) % 10;
-  }
-
-  public static int length(int n){
-    if(n==0){
-      return 0;
+    public static int nth(int n, int col){
+      return (int) (Math.abs(n) / Math.pow(10, col)) % 10;
     }
-    return (int) (Math.log10(Math.abs(n)) + 1);
-  }
 
-  public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
-    for(int i = 0; i < buckets.length; i ++){
-      original.extend(buckets[i]);
+    public static int length(int n){
+      if(n==0){
+        return 0;
+      }
+      return (int) (Math.log10(Math.abs(n)) + 1);
     }
-  }
-  public static void insertionSort(int[] data){
-    for(int i = 1; i < data.length; i ++){
-      int j = i;
-      while(j > 0 && data[j-1]>data[j]){
-        int temp = data[j];
-        data[j]=data[j-1];
-        data[j-1] = temp;
-        j -=1;
+
+    public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
+      for(int i = 0; i < buckets.length; i ++){
+        original.extend(buckets[i]);
       }
     }
-  }
-  public static void radixSortSimple(SortableLinkedList data){
 
-    int maxValue = 0;
-    for(int i = 0; i < data.size(); i ++){ //loop through data to get maxValue
-        maxValue = Math.max(data.get(i),maxValue);
-    }
-    int mostDigits = length(maxValue); //e.g 432, mostdigits would be 3
-    for(int i = 0; i < mostDigits; i ++){
-      SortableLinkedList zero = new SortableLinkedList();
-      SortableLinkedList one = new SortableLinkedList();
-      SortableLinkedList two = new SortableLinkedList();
-      SortableLinkedList three = new SortableLinkedList();
-      SortableLinkedList four = new SortableLinkedList();
-      SortableLinkedList five = new SortableLinkedList();
-      SortableLinkedList six = new SortableLinkedList();
-      SortableLinkedList seven = new SortableLinkedList();
-      SortableLinkedList eight = new SortableLinkedList();
-      SortableLinkedList nine = new SortableLinkedList();    //create buckets everytime, first pass for one's digit, 2nd for 2nd, third for 3rd
+    public static void radixSortSimple(SortableLinkedList data){
 
-    for(int j = 0; j < data.size(); j ++){ //loop through data to sort
-      int ele = data.get(j);
-      int digit = nth(ele,j);  //gets ones place, added according to where. This is a stable sort too. Is there a way to do this with a loop?
-      if(digit ==0) zero.add(ele);
-      else if(digit == 1) one.add(ele);
-      else if(digit == 2) two.add(ele);
-      else if(digit == 3) three.add(ele);
-      else if(digit == 4) four.add(ele);
-      else if(digit == 5) five.add(ele);
-      else if(digit == 6) six.add(ele);
-      else if(digit == 7) seven.add(ele);
-      else if(digit == 8) eight.add(ele);
-      else if(digit == 9) nine.add(ele);
-    }
-    SortableLinkedList temp = new SortableLinkedList();
-    SortableLinkedList[] buckets = new SortableLinkedList[]{zero,one,two,three,four,five,six,seven,eight,nine};
-    merge(temp,buckets);
-    for(int j = 0; j < data.size(); j ++){//removing from data to add in a sorted order
-      data.remove(j);
-    }
-    data.extend(temp);
-  }
+      int maxValue = 0;
+      for(int i = 0; i < data.size(); i ++){ //loop through data to get maxValue
+          if (data.get(i) > maxValue){
+            maxValue = data.get(i);
+          }
+      }
+      int mostDigits = length(maxValue); //e.g 432, mostdigits would be 3
+      SortableLinkedList[] buckets = new SortableLinkedList[10];
+      for(int i = 0; i < 10; i ++){
+        buckets[i] = new SortableLinkedList();
+      }
+
+      for(int i = 0; i < mostDigits; i ++){
+        while(data.size()>0){
+          int ele = data.get(0  );
+          int digit = nth(ele,i);
+          buckets[digit].add(ele);
+          data.remove(0);
+        }
+        merge(data,buckets);
+      }
 
   }
 
-}
+    public static void radixSort(SortableLinkedList data){
+      SortableLinkedList nonNegative = new SortableLinkedList();
+      SortableLinkedList negative = new SortableLinkedList();
+
+      for(int i = 0; i < data.size(); i ++){
+        if(data.get(i)>=0){
+          nonNegative.add(data.get(i));
+        }
+        else{
+          negative.add(data.get(i) * -1);
+        }
+      }
+
+      radixSortSimple(nonNegative);
+      radixSortSimple(negative);
+
+      for(int i = data.size() -1; i >= 0; i--){
+        data.remove(i);
+      }
+      for (int i = negative.size() - 1; i >= 0; i--) {
+        data.add(negative.get(i) * -1);
+      }
+
+      data.extend(nonNegative);
+    }
+
+
+
+  }
